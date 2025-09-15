0df57cc58622703036900b6d92346df64ee41962
jeromejouve-lab <jerome.jouve@gmail.com>
2025-09-15 19:52:49 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import * as Cards     from './ui/tabs/cards.js';
 import * as Scenarios from './ui/tabs/scenarios.js';
 import * as Projector from './ui/tabs/projector.js';
 import * as Journal   from './ui/tabs/journal.js';
-import './core/compat-export.js';
+import './core/compat-exports.js';
 import './core/restore.js';
 
 
@@ -67,6 +67,7 @@ if (document.readyState === 'complete' || document.readyState === 'interactive')
 
 // utile au besoin depuis la console
 try { window.showTab = showTab; window.pariaBoot = boot; } catch {}
+
 
 
 
